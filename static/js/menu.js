92d6b5035cef03292a4510dcd90c9f72c71f6c29
Daniel Calvo <daniel.cal.gl@gmail.com>
2018-09-15 14:07:27 +0200

    Añadido los controles para el jardin y las luces

--- static/js/menu.js
+++ static/js/menu.js
@@ -11,6 +11,13 @@ $('.header-left').on('click', function () {
     $('.menu').toggleClass("menu-show");
 })
 
+$('.fa-bars').on('click', function () {
+  
+    $('.menu').toggleClass("menu-show");
+})
+
+
+
 /*
 var cw = $('.child').width();
 $('.child').css({'height':cw+'px'});
